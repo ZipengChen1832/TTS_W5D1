3eaec1a4398c7ef1cff3882ffd53b0a9a17d6e01
ZipengChen1832 <83133055+ZipengChen1832@users.noreply.github.com>
2021-07-19 21:26:48 -0700

    Add files via upload

---
+++ TTS W5D1/src/W5D1.java
@@ -0,0 +1,16 @@
+public class W5D1 {
+    public static void main(String[] args){
+        int days_in_week = 7;
+        int days_in_January = 31;
+
+        illegal variable names;
+//        String !name = "Zipeng";    //';' expected
+        String 1name = "Zipeng";
+        System.out.println(!name);
+
+//        uninitialized variable
+//        String title;
+//        System.out.println(title);
+//        compiler error: variable title might not have been initialized
+    }
+}
